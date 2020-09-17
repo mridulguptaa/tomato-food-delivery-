7d6c95590b2e56877ca62bbb34c1437d8a948e23
Mridul <mridul15gupta@gmail.com>
2020-09-17 14:55:02 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+{/*import HelloWorld from "./components/HelloWorld/HelloWorld";*/}
+
+function App() {
+  return (
+    <div className="App">
+        <nav class="navbar navbar-dark bg-dark ">
+          <a class="navbar-brand" href="#">
+            <h2 styles="text-align:right">Tomato</h2>
+          </a>
+      </nav>   
+      <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+      <div>
+        <button  type="button" class="btn btn-outline-secondary btn-lg" >I am a Customer</button>
+        
+        <button  type="button" class="btn btn-outline-secondary btn-lg" >I am a Restaurant</button>
+      
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HelloWorld/HelloWorld.js
@@ -0,0 +1,13 @@
+import React,{Component,Fragment} from 'react';
+import "./HelloWorld.css";
+
+const HelloWorld = () =>{
+	return(
+		<div className="HelloWorld">
+			<h1> Hello World </h1>
+			<p> Try a paragraph</p>
+		</div>
+	);
+}
+
+export default HelloWorld;
